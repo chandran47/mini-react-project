e2de870489db0c33174192a7856b1a0cf37db8b1
Chandra Sekhar <chandranallangu7@gmail.com>
2025-07-14 11:35:48 +0530

    Fix: redirect wildcard route to home to avoid 404

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Nav from "./miniproj.jsx/navbar";
 import Home from "./miniproj.jsx/home"
 import Form from "./miniproj.jsx/form"
 import User from "./miniproj.jsx/users"
-import Notfound from "./miniproj.jsx/notfound";
+import { Navigate } from "react-router-dom";
 import Userdetails from "./miniproj.jsx/userdetails";
 import {  BrowserRouter, Routes, Route} from "react-router-dom";
 function Main(){
@@ -17,7 +17,7 @@ return(
 <Route path="/form" element={<Form/>}/>
 <Route path="/users/" element={<User/>}/>
 <Route path="/users/:id" element={<Userdetails/>}/>
-<Route path="*" element={<Notfound/>}/>
+<Route path="*" element={<Navigate to="/" />}/>
 </Route>  
 </Routes>
 
