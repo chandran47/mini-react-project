4311d4cdc01add13b7060f5c7d2b8c535ee30081
Chandra Sekhar <chandranallangu7@gmail.com>
2025-07-16 00:36:00 +0530

    Added search and filter to user list

--- src/miniproj.jsx/users.jsx
+++ src/miniproj.jsx/users.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 function User(){
 
 const[user,setuser]=useState([])
@@ -7,15 +6,33 @@ useEffect(()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
         .then((res)=>res.json())
         .then((data)=>setuser(data))},[])
-
+const[searchit, setsearchit]=useState("")
+const[filterit, setfilterit]=useState("all")
+// searchit!===("")||filterit!===("all")
+const filteredusers=user.filter((one)=>{
+const searched=one.name.toLowerCase().includes(searchit.toLowerCase())
+const filtered=filterit==="all"||one.email===filterit
+return searched && filtered
+}
+)
 return(
     <div>
-        
-        {user.map(n=><p>
-             <Link to={`/users/${n.id}`}>{n.name}</Link>
-            </p>)}
 
-    </div>
-)}
+<input type="text" placeholder="search the name" value={searchit} onChange={(e)=>setsearchit(e.target.value)}/>
+<select value={filterit} onChange={e=>setfilterit(e.target.value)}>
+<option value="all">ALL</option>
+{user.map((i)=>(
+<option value={i.email}>{i.email}</option>
+))}
+     </select>
 
+     <ul>
+        {filteredusers.length>0?filteredusers.map((n)=>(<li><strong>{n.name}</strong> - {n.email}</li>)):<li>no user found</li>
+
+ } </ul>
+
+</div>
+)
+
+}
 export default User
